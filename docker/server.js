6acb3b5e766eaeece0d11fac980d4a360a60451e
*21BrysYavR <takatoshi16@gmail.com>
2022-08-20 21:21:03 +0900

    serverjsを修正

--- docker/server.js
+++ docker/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.use('/img', express.static(__dirname + '/dist/img/'));
-app.use('/css', express.static(__dirname + '/dist/css/'));
-app.use('/js', express.static(__dirname + '/dist/js/'));
-app.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
+app.use('/img', express.static(__dirname + '/img/'));
+app.use('/css', express.static(__dirname + '/css/'));
+app.use('/js', express.static(__dirname + '/js/'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.listen(80, () => console.log('Example app listening on port 80!'))
